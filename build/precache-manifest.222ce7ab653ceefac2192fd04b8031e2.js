4b70d72e66848f9f238bae8ab201b4ca23049056
sarthakarora1208 <sarthakarora1208@gmail.com>
2020-07-23 15:48:25 +0530

    changed build

--- build/precache-manifest.222ce7ab653ceefac2192fd04b8031e2.js
+++ build/precache-manifest.222ce7ab653ceefac2192fd04b8031e2.js
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/static/media/Stellar.5fc50d33.jpg"
   },
   {
-    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
-    "url": "/static/media/nucleo.5987dd12.woff2"
-  },
-  {
     "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
     "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
   },
   {
     "revision": "03ef1918e505c3e3471f9369ef7a638f",
@@ -44,12 +44,12 @@ self.__precacheManifest = [
     "url": "/static/media/nucleo.b17a118e.ttf"
   },
   {
-    "revision": "23fa76237f0066f4ffd8f20e77449887",
-    "url": "/static/media/square3.23fa7623.png"
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/static/media/square2.6619b979.png"
   },
   {
-    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
-    "url": "/static/media/square4.8fdf9ee2.png"
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/static/media/square3.23fa7623.png"
   },
   {
     "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "url": "/static/media/square1.ca257073.png"
   },
   {
-    "revision": "6619b9792d7d108bb589b51bbd560278",
-    "url": "/static/media/square2.6619b979.png"
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
   },
   {
     "revision": "895c5b861871ff6ea1b09a298c9390b5",
